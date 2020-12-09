80f2934000dd30b22ccebeb5ee4cac745cc6a533
Zach Mohammed <zach.mohammed@ada.ac.uk>
2020-12-09 15:21:05 +0000

    removed files

--- U4 Project - 2/main.py
+++ U4 Project - 2/main.py
@@ -1,24 +1,30 @@
-from processing import process_times
+def get_gender():
+    repeat = True
+    while repeat == True:
+        race_gender = input("Are the athletes competing Male or Female (M/F): ").upper()
+        if race_gender == 'M':
+            record_times = {"World Record Time": 9.58, "European Record Time": 9.86, "British Record Time": 9.87}
+            repeat = False
+        elif race_gender == 'F':
+            record_times = {"World Record Time": 10.49, "European Record Time": 10.73, "British Record Time": 10.99}
+            repeat = False
+        else:
+            print("Invalid input, please enter M or F")
+    return record_times
 
 
-race_gender = input("Are the athletes competing Male or Female (M/F): ").upper()
-while race_gender not in ('M', 'F'):
-    print("Invalid input, please enter M or F")
-    race_gender = input("Are the athletes competing Male or Female (M/F): ").upper()
- 
 
+record_times = get_gender()
 adding_race_times = True
 lane_number = 1
 race_times = []
+
 while adding_race_times == True:
-    temp_race_time = input("Time of the athlete on lane " + str(lane_number) + " ,(X: to stop): ")
+    temp_race_time = input(f"Time of the athlete on lane {str(lane_number)} (X to stop): ")
     if temp_race_time.upper() == "X":
         if len(race_times) == 0:
             print("No times where added at all")
-            break;
-        else:
-            process_times(race_times, race_gender)
-            break;
+        adding_race_times = False
     else:
         if temp_race_time.replace('.','',1).isdigit():
             if float(temp_race_time) <= 0:
@@ -29,7 +35,26 @@ while adding_race_times == True:
         else:
             print("Invalid input")
 
-        
+def process_times(race_times, record_times):
+    min_athletes = 4
+    max_athletes = 10
 
+    race_times.sort(key=lambda x:x[1], reverse=False)
 
+    for lane_list in race_times:
+        print(f"Lane {str(lane_list[0])} : {str(lane_list[1])}")
+
+    if len(race_times) < min_athletes:
+        print("The amount of athletes is too small to qualify")
+    elif len(race_times) >= max_athletes:
+        print("The amount of athletes is too large to qualify")
+    else:
+        print("------------------------------")
+        for record_name, record_time in record_times.items():
+            if race_times[0][1] < record_time:
+                print(f"{record_name} beaten at {str(race_times[0][1])} in lane {str(race_times[0][0])}")     
+        print("------------------------------")
+
+if len(race_times) >= 1:
+    process_times(race_times, record_times)
 
